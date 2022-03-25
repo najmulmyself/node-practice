2f67e41f382454062537de3736efa758aaf50abc
najmulmyself <najmul.myself@gmail.com>
2022-03-25 20:35:28 +0600

    practice with raw node

---
+++ RawNode/http.js
@@ -0,0 +1,7 @@
+const http = require('http')
+
+const server = http.createServer((req,res)=>{
+    res.end('<h1>Hello World from the world of Node</h1>')
+})
+
+server.listen(1000,()=> console.log('Server is Listening'))
--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
 const app = express()
 
+app.use(cors())
+app.use(bodyParser.json())
+
+app.post('/', (req,res) => {
+    console.log(req.body)
+})
+
 app.get('/', (req, res) =>{
-    res.send('<h1>Thanks For Sign In , We are Pleased</h1>')
+    const friends = {
+        name : 'John',
+        salary : 300,
+        id : 1,
+    }
+    res.send(friends)
 })
 
 app.listen(3001,()=>{
